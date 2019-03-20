ec6caaaf8c7f2d44753c5bd528e8c8a29dab1800
MichelML <michmoreau.l@gmail.com>
2019-03-20 12:35:21 -0400

    add head for home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
+import Head from 'next/head'
+
 function Home() {
-  return <div>Welcome</div>
+  const name = 'Home';
+  return (
+    <div>
+      <Head>
+        <title>michelml - {name}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+      </Head>
+    </div>
+  );
 }
 
-export default Home
+export default withTheme()(Home)
