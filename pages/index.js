ee0f8d9629d71c1b34dbd1d9dd5af0ca2c68856d
MichelML <michmoreau.l@gmail.com>
2019-03-30 15:54:18 -0400

    add headerbar

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
 import Head from 'next/head'
+import HeaderBar from '../components/HeaderBar';
 
 function Home() {
   const name = 'Home';
   return (
     <div>
       <Head>
-        <title>michelml - {name}</title>
+        <title>Michel ML - {name}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+        <link rel="stylesheet" href="static/normalize.css" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
       </Head>
+      <HeaderBar />
     </div>
   );
 }
