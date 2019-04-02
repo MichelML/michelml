05e2df3226073f0a3b396774040442951af3cd42
MichelML <michmoreau.l@gmail.com>
2019-04-02 17:13:23 -0400

    make browser bar white

--- components/GenericHead.js
+++ components/GenericHead.js
@@ -5,7 +5,7 @@ const GenericHead = ({name}) => (
     <title>Michel ML - {name}</title>
     <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
     <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-    <meta name="theme-color" content="#000" />
+    <meta name="theme-color" content="#fff" />
     <link rel="stylesheet" href="static/normalize.css"/>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"/>
   </Head>
