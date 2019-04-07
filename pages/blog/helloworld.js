6638b7fec06b8006c1f7f5accaa044ef15ef90cc
MichelML <michmoreau.l@gmail.com>
2019-04-07 14:12:27 -0400

    commit progress on blog post

--- pages/blog/helloworld.js
+++ pages/blog/helloworld.js
@@ -1,10 +1,10 @@
 import React from "react";
+import moment from "moment";
 import post from "../../blog_posts/helloworld.json";
 import { compose } from "lodash/fp";
 import decorate from "../../hoc/decorate";
 import { withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
-import Grid from "@material-ui/core/Grid";
 import classNames from "classnames";
 import assetUrl from "../../utils/assetUrl.js";
 
@@ -13,7 +13,12 @@ const styles = theme => ({
     width: "auto",
     marginLeft: theme.spacing.unit * 3,
     marginRight: theme.spacing.unit * 3,
-    marginTop: theme.spacing.unit * 4
+    marginTop: theme.spacing.unit * 4,
+    [theme.breakpoints.up(900 + theme.spacing.unit * 3 * 2)]: {
+      width: 900,
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
   },
   postImage: {
     minWidth: "100%",
@@ -33,12 +38,18 @@ function Post(props) {
   return (
     <article className={classNames(classes.layout)}>
       <div className={classes.postImage} />
-      <div>
-        <Typography variant="h3" component="h1" align="left">
-          {post.name}
-        </Typography>
-      </div>
-
+      <Typography variant="h3" component="h1" align="left">
+        {post.name}
+      </Typography>
+      <Typography
+        gutterBottom
+        variant="subheading"
+        component="h6"
+        color="textSecondary"
+        align="left"
+      >
+        {post.author} - {moment(post.date).format("MMMM Do YYYY")}
+      </Typography>
       <Typography
         variant="body1"
         align="left"
