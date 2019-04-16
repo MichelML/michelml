154387e4ac225da5e866a2788d7a25b01a4211b4
MichelML <michmoreau.l@gmail.com>
2019-04-16 12:09:22 -0400

    add books

---
+++ pages/library/thinkinginhtml.js
@@ -0,0 +1,13 @@
+import Book from "../../components/Book";
+import book from "../../library/thinkinginhtml.json";
+import React from "react";
+import { compose } from "lodash/fp";
+import decorate from "../../hoc/decorate";
+
+function BookPage() {
+  return <Book book={book} />
+}
+
+export default compose(
+  decorate({ name: "Book - " + book.volumeInfo.title }),
+)(BookPage);
