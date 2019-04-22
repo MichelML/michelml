2855fa4a9f3e058cc789f6ba60caddd222fa2dc4
MichelML <michmoreau.l@gmail.com>
2019-04-21 21:39:34 -0400

    add blog post

---
+++ blog_posts/tenguidelinesformaintainablecodefromjoostvisseretalia.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const moment = require("moment");
+
+module.exports = {
+  name: "ten guidelines for maintainable code from Joost Visser et alia",
+  cleanName: "tenguidelinesformaintainablecodefromjoostvisseretalia",
+  author: "Michel Moreau",
+  date: "2019-04-21T00:00:00-04:00",
+  post: fs.readFileSync("blog_posts/tenguidelinesformaintainablecodefromjoostvisseretalia.html", "utf8"),
+  img: "/static/postimages/tenguidelinesformaintainablecodefromjoostvisseretalia.jpg",
+  svg: "/static/postimages/tenguidelinesformaintainablecodefromjoostvisseretalia.svg"
+};
