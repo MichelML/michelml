102602deb35a98663e8408acab03aa3f5a5d7bc4
MichelML <michmoreau.l@gmail.com>
2019-04-21 21:54:26 -0400

    add post

---
+++ blog_posts/theonlyrealandtimelessdefinitionofsuccess.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const moment = require("moment");
+
+module.exports = {
+  name: "the only real and timeless definition of success",
+  cleanName: "theonlyrealandtimelessdefinitionofsuccess",
+  author: "Michel Moreau",
+  date: "2019-04-21T00:00:00-04:00",
+  post: fs.readFileSync("blog_posts/theonlyrealandtimelessdefinitionofsuccess.html", "utf8"),
+  img: "/static/postimages/theonlyrealandtimelessdefinitionofsuccess.jpg",
+  svg: "/static/postimages/theonlyrealandtimelessdefinitionofsuccess.svg"
+};
