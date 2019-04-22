9a4533a70fb1c3681d20080a58698fd4f34ad309
MichelML <michmoreau.l@gmail.com>
2019-04-21 21:18:44 -0400

    add blog post

---
+++ blog_posts/howtomasteranyskill.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const moment = require("moment");
+
+module.exports = {
+  name: "How to master any skill",
+  cleanName: "howtomasteranyskill",
+  author: "Michel Moreau",
+  date: "2019-04-21T00:00:00-04:00",
+  post: fs.readFileSync("blog_posts/howtomasteranyskill.html", "utf8"),
+  img: "/static/postimages/howtomasteranyskill.jpg",
+  svg: "/static/postimages/howtomasteranyskill.svg"
+};
