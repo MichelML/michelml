3c3f23c35b21ff5a5a9eed87a01bff44d05658cc
MichelML <michmoreau.l@gmail.com>
2019-04-23 13:34:57 -0400

    add firebase

--- components/GenericHead.js
+++ components/GenericHead.js
@@ -40,18 +40,24 @@ const GenericHead = ({ name }) => (
     <meta name="theme-color" content="#fff" />
     <link rel="preload" href={assetUrl("static/styles.css")} as="style" />
     <link rel="stylesheet" href={assetUrl("static/styles.css")} />
+    {/* Google Analytics */}
     <script
       defer
       src="https://www.googletagmanager.com/gtag/js?id=UA-138306187-1"
     />
     <script src={assetUrl("static/google_analytics.js")} />
+    {/* Google Signin */}
     <script src="https://apis.google.com/js/platform.js" async defer />
     <script src={assetUrl("static/google_signin.js")} />
+    {/* Google Ads */}
     <script
       async
       src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
     />
     <script src={assetUrl("static/google_ads.js")} />
+    {/* Firebase */}
+    <script src="https://www.gstatic.com/firebasejs/5.10.0/firebase.js" />
+    <script src={assetUrl("static/google_firebase.js")} />
   </Head>
 );
 
---
+++ static/google_firebase.js
@@ -0,0 +1,9 @@
+  var config = {
+    apiKey: "AIzaSyCe4c379-ilCKTPKSLhE9KTL7152y5PqTY",
+    authDomain: "michel-ml.firebaseapp.com",
+    databaseURL: "https://michel-ml.firebaseio.com",
+    projectId: "michel-ml",
+    storageBucket: "michel-ml.appspot.com",
+    messagingSenderId: "924976100568"
+  };
+  firebase.initializeApp(config);
