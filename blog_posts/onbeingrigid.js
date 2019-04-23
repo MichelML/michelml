3a7daa1591c43360714063451961459aa36fbafe
MichelML <michmoreau.l@gmail.com>
2019-04-23 11:21:18 -0400

    add post

---
+++ blog_posts/onbeingrigid.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+module.exports = {
+  name: "On Being Rigid",
+  cleanName: "onbeingrigid",
+  author: "Michel Moreau",
+  date: "2019-04-23T00:00:00-04:00",
+  post: fs.readFileSync("blog_posts/onbeingrigid.html", "utf8"),
+  img: "/static/postimages/onbeingrigid.jpg",
+  svg: "/static/postimages/onbeingrigid.svg"
+};
