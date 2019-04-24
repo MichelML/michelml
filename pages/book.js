489d234008782f93d22f5099dcb60235d83cbb50
MichelML <michmoreau.l@gmail.com>
2019-04-24 14:08:02 -0400

    add server side rendered pages for books and posts

--- pages/book.js
+++ pages/book.js
@@ -8,10 +8,11 @@ import Typography from "@material-ui/core/Typography";
 import striptags from "striptags";
 import { withStyles } from "@material-ui/core/styles";
 import assetUrl from "../utils/assetUrl";
-import urlJoin from "url-join";
-import { withRouter } from "next/router";
 import decorate from "../hoc/decorate";
 import Head from "next/head";
+import Link from "next/link";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import Button from "@material-ui/core/Button";
 
 const styles = theme => ({
   layout: {
@@ -47,82 +48,107 @@ const styles = theme => ({
   },
   bookInfo: {
     marginTop: theme.spacing.unit * 4
+  },
+  browseBooksMargin: {
+    marginTop: theme.spacing.unit * 4
   }
 });
 
-class Book extends React.Component {
-  state = {
-    book: null
-  };
+function Book(props) {
+  const { classes, book } = props;
 
-  async componentDidMount() {
-    try {
-      const book = await require(`../library/${
-        this.props.router.query.book
-      }.json`);
-      this.setState({ book });
-    } catch (e) {
-      this.props.router.replace(urlJoin(location.origin, "404"));
-    }
-  }
+  const BookNotFound = () => (
+    <div>
+      <Head>
+        <title>Michel ML - Book Not Found</title>
+      </Head>
+      <main className={classNames(classes.layout)}>
+        <Typography gutterBottom variant="h3" component="h1" align="left">
+          Book Not Found.
+        </Typography>
+        <Typography variant="body1" align="left">
+          We were unable to find the requested book.
+        </Typography>
+        <Link href="/library">
+          <Button className={classes.browseBooksMargin} color="primary">
+            <ArrowBackIcon />
+            Browse other books
+          </Button>
+        </Link>
+      </main>
+    </div>
+  );
 
-  render() {
-    const { classes } = this.props;
-    const { book } = this.state;
-    return (
-      this.state.book && (
-        <>
-          <Head>
-            <title>Michel ML - Book - {book.volumeInfo.title}</title>
-          </Head>
-          <article className={classNames(classes.layout)}>
-            <Card className={classes.card}>
-              <CardContent className={classes.cardMediaContainer}>
-                <CardMedia
-                  className={classes.cardMedia}
-                  image={
-                    book.volumeInfo.imageLinks
-                      ? assetUrl(book.volumeInfo.imageLinks.smallThumbnail, {
-                          external: true
-                        })
-                      : assetUrl("static/product_image_not_found.gif")
-                  }
-                  title={book.volumeInfo.title}
-                />
-              </CardContent>
-              <CardContent className={classes.cardContent}>
-                <Typography gutterBottom variant="body1" component="h2">
-                  {book.volumeInfo.title}
-                </Typography>
-                <Typography
-                  gutterBottom
-                  variant="body2"
-                  component="h6"
-                  color="textSecondary"
-                >
-                  {(book.volumeInfo.authors &&
-                    book.volumeInfo.authors.join(", ")) ||
-                    "Unknown author"}
-                </Typography>
-              </CardContent>
-            </Card>
-            <div className={classes.bookInfo}>
-              <Typography gutterBottom variant="subtitle1">
-                Description
-              </Typography>
-              <Typography variant="body2">
-                {striptags(book.volumeInfo.description || "No description")}
-              </Typography>
-            </div>
-          </article>
-        </>
-      )
-    );
-  }
+  return !book ? (
+    <BookNotFound />
+  ) : (
+    <div>
+      <Head>
+        <title>Michel ML - Book - {book.volumeInfo.title}</title>
+      </Head>
+      <article className={classNames(classes.layout)}>
+        <Card className={classes.card}>
+          <CardContent className={classes.cardMediaContainer}>
+            <CardMedia
+              className={classes.cardMedia}
+              image={
+                book.volumeInfo.imageLinks
+                  ? assetUrl(book.volumeInfo.imageLinks.smallThumbnail, {
+                      external: true
+                    })
+                  : assetUrl("static/product_image_not_found.gif")
+              }
+              title={book.volumeInfo.title}
+            />
+          </CardContent>
+          <CardContent className={classes.cardContent}>
+            <Typography gutterBottom variant="body1" component="h2">
+              {book.volumeInfo.title}
+            </Typography>
+            <Typography
+              gutterBottom
+              variant="body2"
+              component="h6"
+              color="textSecondary"
+            >
+              {(book.volumeInfo.authors &&
+                book.volumeInfo.authors.join(", ")) ||
+                "Unknown author"}
+            </Typography>
+          </CardContent>
+        </Card>
+        <div className={classes.bookInfo}>
+          <Typography gutterBottom variant="subtitle1">
+            Description
+          </Typography>
+          <Typography variant="body2">
+            {striptags(book.volumeInfo.description || "No description")}
+          </Typography>
+        </div>
+        <Link href="/library">
+          <Button className={classes.browseBooksMargin} color="primary">
+            <ArrowBackIcon />
+            Browse other books
+          </Button>
+        </Link>
+      </article>
+    </div>
+  );
 }
 
-export default compose(
-  withRouter,
+Book = compose(
   decorate({ name: "Book", ads: true }),
   withStyles(styles)
 )(Book);
+
+Book.getInitialProps = async ({ query }) => {
+  let book;
+  try {
+    book = await require(`../library/${query.book}.json`);
+  } catch (e) {
+    book = null;
+  }
+  return { book };
+};
+
+export default Book;
