d610effc6efcf8c76c481b4b815a5695f90ae599
MichelML <michmoreau.l@gmail.com>
2019-04-30 12:11:30 -0400

    fix script

--- scripts/blogposts/aggregate_posts.js
+++ scripts/blogposts/aggregate_posts.js
@@ -11,8 +11,9 @@ const writeFile = (filePath, file, content) => {
   fs.writeFileSync(
     path.join(filePath, file),
     JSON.stringify(content, null, 4),
-    "utf8"
+    { encoding: "utf8"}
   );
+
 };
 
 const sortByDate = (postA, postB) => {
@@ -23,13 +24,13 @@ const sortByDate = (postA, postB) => {
 
 const allBlogPosts = files
   .filter(file => /\.js$/.test(file))
-  .map(file => file.replace(/\.js$/, ".json"))
-  .reduce((allPosts, file) => {
-    const blogPost = require(path.join(blogpostsPath, file));
-    writeFile(blogpostsPath, file, blogPost);
-    writeFile(staticBlogpostsPath, file, blogPost);
-    return [...allPosts, blogPost];
-  }, [])
+  .map(file => ({js: file, json: file.replace(/\.js$/, ".json")}))
+  .map(file => {
+    const blogPost = require(path.join(blogpostsPath, file.js));
+    writeFile(blogpostsPath, file.json, blogPost);
+    writeFile(staticBlogpostsPath, file.json, blogPost);
+    return blogPost;
+  })
   .sort(sortByDate);
 
 writeFile(process.cwd(), "recentPosts.json", take(allBlogPosts, 3));
