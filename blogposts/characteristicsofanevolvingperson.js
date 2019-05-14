9b4e23dff09ca812036b5735f23e0f5a35ce244e
MichelML <michmoreau.l@gmail.com>
2019-05-14 17:03:04 -0400

    lint

--- blogposts/characteristicsofanevolvingperson.js
+++ blogposts/characteristicsofanevolvingperson.js
@@ -5,7 +5,10 @@ module.exports = {
   cleanName: "characteristicsofanevolvingperson",
   author: "Michel Moreau",
   date: "2019-05-14T00:00:00-04:00",
-  post: fs.readFileSync("blogposts/characteristicsofanevolvingperson.html", "utf8"),
+  post: fs.readFileSync(
+    "blogposts/characteristicsofanevolvingperson.html",
+    "utf8"
+  ),
   img: "/static/postimages/characteristicsofanevolvingperson.svg",
   svg: "/static/postimages/characteristicsofanevolvingperson.svg"
 };
